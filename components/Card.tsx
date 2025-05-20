420e305d446f87c2834009b1d76a05d64e32ea10
brayanriuge <145686424+brayanriunge@users.noreply.github.com>
2025-05-20 15:43:05 +0300

    Card designing

--- components/Card.tsx
+++ components/Card.tsx
@@ -4,23 +4,23 @@ import file from "@/public/file.svg";
 export default function Card() {
   return (
     <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-4">
-      <div className="flex flex-col  hover:shadow-lg transition-shadow duration-300 bg-white border border-gray-200">
+      <div className="flex flex-col  hover:shadow-lg transition-shadow duration-300 bg-white border border-gray-200 rounded">
         <Image src={file} alt="file" width={400} height={400} />
         <div className="flex-grow px-4 py-3">
-          <h1 className=" font-bold text-black text-2xl">
+          <h1 className="  text-black font-bold text-[28px] ">
             MADARAKA VIEW ESTATE
           </h1>
-          <p>
-            <span className="text-lg">Cash Price:</span>
+          <p className="mt-4 text-lg">
+            Cash Price: <span className="text-[#1E90FF]">100K</span>
           </p>
-          <p>
-            <span className="text-lg">Plot size:</span>
+          <p className="mt-4 text-lg ">
+            Plot size: <span className="text-[#1E90FF]">1/8</span>
           </p>
-          <p>
-            <span className="text-lg">Location:</span>
+          <p className="mt-4 text-lg">
+            Location: <span className="text-[#1E90FF]"> Madaraka</span>
           </p>
-          <p>
-            <span className="text-lg">Installment Price:</span>
+          <p className="mt-4 text-lg">
+            Installment Price:<span className="text-[#1E90FF]"> 100K</span>
           </p>
         </div>
       </div>
