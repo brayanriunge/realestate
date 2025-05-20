c224e97fe2877b3195c6f2f2aa50fde7e14fef73
brayanriuge <145686424+brayanriunge@users.noreply.github.com>
2025-05-20 14:53:06 +0300

    Card properties

--- components/Card.tsx
+++ components/Card.tsx
@@ -5,7 +5,24 @@ export default function Card() {
   return (
     <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-4">
       <div className="flex flex-col  hover:shadow-lg transition-shadow duration-300 bg-white border border-gray-200">
-        <Image src={file} alt="file" width={90} height={90} />
+        <Image src={file} alt="file" width={400} height={400} />
+        <div className="flex-grow px-4 py-3">
+          <h1 className=" font-bold text-black text-2xl">
+            MADARAKA VIEW ESTATE
+          </h1>
+          <p>
+            <span className="text-lg">Cash Price:</span>
+          </p>
+          <p>
+            <span className="text-lg">Plot size:</span>
+          </p>
+          <p>
+            <span className="text-lg">Location:</span>
+          </p>
+          <p>
+            <span className="text-lg">Installment Price:</span>
+          </p>
+        </div>
       </div>
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,23 @@
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
 import Head from "next/head";
 import Navbar from "@/components/Navbar";
 import Card from "@/components/Card";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title className={`${geistMono.className}`}>Home</title>
+        <title>Home</title>
         <meta property="og:title" content="My page title" key="title" />
         <link rel="icon" href="/LOGO.jpg" />
       </Head>
