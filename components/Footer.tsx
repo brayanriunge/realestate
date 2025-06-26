f1dd908fe8981f0f20fea9bf08e40b1906842e38
brayanriuge <145686424+brayanriunge@users.noreply.github.com>
2025-06-26 20:27:06 +0300

    Footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,7 @@
 export default function Footer() {
   return (
-    <div className="mx-auto w-full flex items-center justify-between  ">
+    <div className="mx-auto w-full flex flex-row items-center justify-between  ">
+      {/* location */}
       <div className=" flex flex-col gap-4 ">
         <p>
           loction:{" "}
@@ -8,6 +9,8 @@ export default function Footer() {
         </p>
         <p></p>
       </div>
+      {/* Contacts */}
+      <div className="flex flex-col justify-between items-center gap-4"></div>
     </div>
   );
 }
